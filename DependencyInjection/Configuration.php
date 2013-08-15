9312747493a3057bf32068b931ded39cb61d087e
epidoux <eric.pidoux@gmail.com>
2013-08-16 00:04:06 +0200

    fix tree builder

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -63,9 +63,9 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('epidoux_oauth');
+        $rootNode = $treeBuilder->root('epidoux_oauth_external');
 
-        $rootNode
+       /* $rootNode
             ->children()
                 ->scalarNode('callback_url')->isRequired()->end()
                 ->arrayNode('services')
@@ -135,7 +135,7 @@ class Configuration implements ConfigurationInterface
 
                         ->end()
                 ->end()
-            ->end();
+            ->end();*/
 
         return $treeBuilder;
     }
