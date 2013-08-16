7a2f59bf55d84f67679e3411e9cf9ba55dceb871
epidoux <eric.pidoux@gmail.com>
2013-08-16 16:22:19 +0200

    fix provider factory

--- Factories/ProviderFactory.php
+++ Factories/ProviderFactory.php
@@ -3,46 +3,6 @@
 namespace Epidoux\OAuthExternalBundle\Factories;
 
 use Epidoux\OAuthExternalBundle\OAuth\OauthClient;
-use Epidoux\OAuthExternalBundle\Entities\Connectors\AmazonConnector;
-use Epidoux\OAuthExternalBundle\Entities\Connectors\BitbucketConnector;
-use Epidoux\OAuthExternalBundle\Entities\Connectors\BitlyConnector;
-use Epidoux\OAuthExternalBundle\Entities\Connectors\BoxConnector;
-use Epidoux\OAuthExternalBundle\Entities\Connectors\DailymotionConnector;
-use Epidoux\OAuthExternalBundle\Entities\Connectors\DeviantartConnector;
-use Epidoux\OAuthExternalBundle\Entities\Connectors\DisqusConnector;
-use Epidoux\OAuthExternalBundle\Entities\Connectors\DropboxConnector;
-use Epidoux\OAuthExternalBundle\Entities\Connectors\EventfulConnector;
-use Epidoux\OAuthExternalBundle\Entities\Connectors\EvernoteConnector;
-use Epidoux\OAuthExternalBundle\Entities\Connectors\FacebookConnector;
-use Epidoux\OAuthExternalBundle\Entities\Connectors\FitbitConnector;
-use Epidoux\OAuthExternalBundle\Entities\Connectors\FlickrConnector;
-use Epidoux\OAuthExternalBundle\Entities\Connectors\FoursquareConnector;
-use Epidoux\OAuthExternalBundle\Entities\Connectors\GithubConnector;
-use Epidoux\OAuthExternalBundle\Entities\Connectors\GoogleConnector;
-use Epidoux\OAuthExternalBundle\Entities\Connectors\InstagramConnector;
-use Epidoux\OAuthExternalBundle\Entities\Connectors\OAuth1Connector;
-use Epidoux\OAuthExternalBundle\Entities\Connectors\OAuth1aConnector;
-use Epidoux\OAuthExternalBundle\Entities\Connectors\OAuth2Connector;
-use Epidoux\OAuthExternalBundle\Entities\Connectors\OdnoklassnikiConnector;
-use Epidoux\OAuthExternalBundle\Entities\Connectors\QqConnector;
-use Epidoux\OAuthExternalBundle\Entities\Connectors\RightSignatureConnector;
-use Epidoux\OAuthExternalBundle\Entities\Connectors\SalesforceConnector;
-use Epidoux\OAuthExternalBundle\Entities\Connectors\ScoopitConnector;
-use Epidoux\OAuthExternalBundle\Entities\Connectors\SensioConnectConnector;
-use Epidoux\OAuthExternalBundle\Entities\Connectors\SinaWeiboConnector;
-use Epidoux\OAuthExternalBundle\Entities\Connectors\StackExchangeConnector;
-use Epidoux\OAuthExternalBundle\Entities\Connectors\StereomoodConnector;
-use Epidoux\OAuthExternalBundle\Entities\Connectors\StockTwitsConnector;
-use Epidoux\OAuthExternalBundle\Entities\Connectors\SurveyMonkeyConnector;
-use Epidoux\OAuthExternalBundle\Entities\Connectors\ThirtySevenSignalsConnector;
-use Epidoux\OAuthExternalBundle\Entities\Connectors\TumblrConnector;
-use Epidoux\OAuthExternalBundle\Entities\Connectors\TwitterConnector;
-use Epidoux\OAuthExternalBundle\Entities\Connectors\VkontaktConnector;
-use Epidoux\OAuthExternalBundle\Entities\Connectors\WindowsLiveConnector;
-use Epidoux\OAuthExternalBundle\Entities\Connectors\WordpressConnector;
-use Epidoux\OAuthExternalBundle\Entities\Connectors\XingConnector;
-use Epidoux\OAuthExternalBundle\Entities\Connectors\YahooConnector;
-use Epidoux\OAuthExternalBundle\Entities\Connectors\YandexConnector;
 
 /**
  * Class ProviderFactory which creates the providers
@@ -111,7 +71,9 @@ class ProviderFactory {
     public function createConnector($name,$service)
     {
         $classname = $this->connectorsName[$service['type']];
-        $connector = new $classname();
+        $namespace = "Epidoux\OAuthExternalBundle\Entities\Connectors\";
+        $class_total = $namespace.$classname;
+        $connector = new $class_total();
         //Generate the url redirect path
         $connector->setName($service['name']);
         $connector->setConfig($service);
